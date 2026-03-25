86bf07ce42cc2b829063fb3b595504d483a7a41c
Cameron Jo'van <83127748+cameron-jovan@users.noreply.github.com>
2026-03-25 10:54:59 -0400

    build logs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
+/*  experimental: {
     turbopack: {
       root: '.'
     }
-  },
+  }, */
   images: {
     unoptimized: true,
   },
